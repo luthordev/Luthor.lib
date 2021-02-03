35e26043806582ea46c3b4a981a3a171cce7ae77
Luthfi Athorique Ginanjar Putra <luthfiathorique@gmail.com>
2021-02-03 12:57:49 +0700

    penambahan enkripsi sha256

---
+++ Luthor.Lib/Encryption/Sha256.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Luthor.lib.Encryption
+{
+    public class Sha256
+    {
+        public static string Encrypt(string text)
+        {
+            SHA256 encrypt = SHA256.Create();
+            return GetHash(encrypt, text);
+        }
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
